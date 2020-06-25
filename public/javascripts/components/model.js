9f658a1ee3a7795723897b9275000d07f7fc2708
Kangseohyun <kshyeon123@gmail.com>
2020-06-25 11:22:06 +0900

    Banner Upload Page

--- public/javascripts/components/model.js
+++ public/javascripts/components/model.js
@@ -4,8 +4,8 @@ class Admin {
         return new Promise(
             async (resolve, reject) => {
                 try {
-                    var resReturn = await myConnection.query('SELECT *, DATE_FORMAT(date, "%Y-%m-%d") AS date FROM ts_notice');
-                    resolve(resReturn[0])
+                    var resReturn = await myConnection.query('SELECT *, DATE_FORMAT(date, "%Y-%m-%d") AS edate FROM notice');
+                    resolve(resReturn)
                 } catch (err) {
                     reject(err)
                 }
@@ -18,10 +18,10 @@ class Admin {
             async (resolve, reject) => {
                 try {
                     var ymd = await functions.DateCreator();
-                    var returnCount = await myConnection.query('SELECT LPAD(COUNT(*) + 1,3,"0") AS cnt FROM ts_notice');
+                    var returnCount = await myConnection.query('SELECT LPAD(COUNT(*) + 1,3,"0") AS cnt FROM notice');
                     var code = 'Notice' + returnCount[0][0].cnt;
 
-                    await myConnection.query('INSERT INTO ts_notice (noticeid, title, date, wel, sentence_1, sentence_2, bye) VALUES (?, ?, ?, ?, ?, ?, ?)', [code, data.title, ymd, data.wel, data.sentence_1, data.sentence_2, data.bye])
+                    await myConnection.query('INSERT INTO notice (noticeid, title, date, wel, sentence_1, sentence_2, bye) VALUES (?, ?, ?, ?, ?, ?, ?)', [code, data.title, ymd, data.wel, data.sentence_1, data.sentence_2, data.bye])
 
                     resolve(true)
                 } catch (err) {
@@ -35,7 +35,7 @@ class Admin {
         return new Promise(
             async (resolve, reject) => {
                 try {
-                    var resReturn = await myConnection.query('SELECT catesid, name FROM ts_cates');
+                    var resReturn = await myConnection.query('SELECT catesid, name FROM cates');
                     resolve(resReturn)
                 } catch (err) {
                     reject * (err)
@@ -48,11 +48,11 @@ class Admin {
         return new Promise (
             async (resolve, reject) => {
                 try {
-                    var returnCount = await myConnection.query('SELECT LPAD(COUNT(*) + 1) AS cnt FROM ts_cates');
+                    var returnCount = await myConnection.query('SELECT LPAD(COUNT(*) + 1) AS cnt FROM cates');
                     var code = 'C' + returnCount[0].cnt;
                     await myConnection.query('INSERT INTO ts_cates (catesid, name) VALUES (?, ?)', [code, name])
                 } catch (err) {
-                    
+                    reject(err)
                 }
             }
         )
@@ -61,11 +61,22 @@ class Admin {
         return new Promise (
             async (resolve, reject) => {
                 try {
-                    await myConnection.query('SELECT * FROM ts_adsuri');
+                    await myConnection.query('SELECT * FROM adsuri');
 
                     resolve()
                 } catch (err) {
                     reject(err)
+                }
+            }
+        )
+    }
+    InsertAds (uri) {
+        return new Promise (
+            async (resolve, reject) => {
+                try {
+                    await myConnection.query('INSERT INTO ts_adsuri (uri) VALUES (?)', [uri]);
+                } catch (err) {
+
                 }
             }
         )
--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,54 @@
 var express = require('express');
+var multer = require('multer');
+const upload = multer({
+  storage: multer.diskStorage({
+    // set a localstorage destination
+    destination: (req, file, cb) => {
+      cb(null, 'public/images/');
+    },
+    // convert a file name
+    filename: (req, file, cb) => {
+      var type = file.mimetype.split('/');
+      cb(null, JSON.stringify(Date.now()) + '.' + type[1]);
+    },
+  }),
+});
 var router = express.Router();
-
 var model = require('../public/javascripts/components/model.js');
+
+router.get('/', (req, res) => {
+  console.log('hi')
+  res.status(200).render('index');
+});
+
+router.post('/login', (req, res) => {
+  res.status(200).render('main');
+});
+
+router.get('/banner', (req, res) => {
+  res.status(200).render('banner');
+});
+
+router.post('/image/upload', upload.single('image'), async (req, res) => {
+  try {
+    console.log(req.body)
+    console.log(req.file)
+
+    var uri = 'http://54.248.0.228:3001/images/' + req.file.filename;
+    console.log(uri)
+    res.status(200).send(true);
+  } catch (err) {
+    console.log(err)
+  }
+});
+
+router.get('/image/load/', async (req, res) => {
+  try {
+
+  } catch (err) {
+
+  }
+})
 
 router.get('/getcates', async (req, res) => {
   try {
@@ -26,9 +73,9 @@ router.post('/insertcates', async (req, res) => {
 router.get('/getnotice', async (req, res) => {
   try {
     var resReturn = await model.GetAllNotice();
-    console.log(resReturn[0].date)
+    console.log(resReturn)
     for (var i = 0; i < resReturn.length; i++) {
-      resReturn[i].date = resReturn[i].date.substring(0, 10)
+      resReturn[i].date = JSON.stringify(resReturn[i].date).substring(1, 11)
       resReturn[i].visible = false;
     }
     res.status(200).send(resReturn)
